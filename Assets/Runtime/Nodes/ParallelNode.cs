c4867efbdb01ca9d95d597d5621c09a7c3237881
fedosklyar <fedos.sklyar@gmail.com>
2026-05-17 17:10:43 +0200

    Adds Parallel Node type

---
+++ Assets/Runtime/Nodes/ParallelNode.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ParallelNode : CompositeNode
+{
+    public int successThreshold = 1;
+    protected override void OnStart()
+    {
+        
+    }
+
+    protected override void OnStop()
+    {
+        
+    }
+
+    protected override State OnUpdate()
+    {
+        int childrenCount = this.children.Count;
+
+        int successCount = 0;
+        int failureCount = 0;
+
+        for(int i = 0; i < childrenCount; i++)
+        {
+            var childState = children[i].Update();
+            if(childState == State.Success)
+                ++successCount;
+            else if(childState == State.Failure)
+                ++failureCount;
+        }
+
+        if(successCount >= successThreshold)
+            return State.Success;
+
+        if(failureCount > (childrenCount - successThreshold))
+            return State.Failure;
+
+        return State.Running;
+    }
+}
